ae53ae11c589de8e39f323b1b0e8b227d9269e7c
Rachel <racheljhumes@gmail.com>
2023-03-08 19:56:53 -0700

    Practice using TS with event props

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { PersonList } from "./components/PersonList";
 import { Status } from "./components/Status";
 import { Heading } from "./components/Heading";
 import { Oscar } from "./components/Oscar";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
 function App() {
   const personName = {
@@ -36,6 +38,15 @@ function App() {
       <Greeting name="Rachel" isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Button
+        handleClick={(event, id) =>
+          console.log("I have been clicked, ouch!", event, id)
+        }
+      />
+      <Input
+        value=""
+        handleChange={(event) => console.log(event.target.value)}
+      />
     </div>
   );
 }
